c588919e8ab7a8b187914d47506b5626e1c4e12a
Justin Campbell <justin@justincampbell.me>
2013-10-15 15:37:45 -0400

    Array implementation

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,5 +1,19 @@
 class Stack
+  attr_reader :array
+
+  def initialize
+    @array = Array.new
+  end
+
+  def push(object)
+    array.push object
+  end
+
+  def pop
+    array.pop
+  end
+
   def empty?
-    true
+    array.empty?
   end
 end
