c0baa90d0a40914cc8731d812305cba3016e3972
Justin Campbell <justin@justincampbell.me>
2013-10-15 15:35:08 -0400

    Linked-list implementation

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,5 +1,31 @@
 class Stack
+  class List
+    attr_reader :head, :tail
+
+    def initialize(head, tail = nil)
+      @head, @tail = head, tail
+    end
+
+    def to_a
+      [head, tail]
+    end
+  end
+
+  attr_reader :list
+
+  def pop
+    head, tail = *list
+
+    @list = tail
+
+    head
+  end
+
+  def push(object)
+    @list = List.new(object, list)
+  end
+
   def empty?
-    true
+    list == nil
   end
 end
--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -6,4 +6,17 @@ describe Stack do
   it "is empty by default" do
     expect(stack).to be_empty
   end
+
+  it "pushes and pops" do
+    stack.push :foo
+
+    expect(stack).to_not be_empty
+
+    stack.push :bar
+
+    expect(stack.pop).to eq(:bar)
+    expect(stack.pop).to eq(:foo)
+
+    expect(stack).to be_empty
+  end
 end
